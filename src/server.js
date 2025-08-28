ba727296a72a0bf6d629d26c1414740da62fe6b5
Your Name <oigor8206@gmail.com>
2025-08-28 16:06:49 -0300

    aula certa

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 const app = express();
-app.get("/html", (req,res)=> {
-    res.send("<h1>teste</h1><p>paragrafo</>");
+
+app.get("/", (req, res) => {
+    res.send("olá mundo!!!");
 });
-app.listen(3000, () =>
-    console.log('Servidor rodando em http://localhost:3000')
+
+app.get("/html", (req, res) => {
+    res.send("<h1>teste</h1><p>Parágrafo</p>");
+});
+
+app.listen(3000, () => 
+    console.log(`Servidor rodando em http://localhost:3000`)
 );

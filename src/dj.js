b1b7b5113e575d6d1a0ece4e472bcf11f19390b4
Your Name <oigor8206@gmail.com>
2025-08-29 14:33:31 -0300

    aula de hj certinho

---
+++ src/dj.js
@@ -0,0 +1,16 @@
+// o objtivo deste arquivo é criar a variável pool
+// e colocar dentro dela a conexão com o postgres
+// Estamos assim vriando um mini pacote 
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+});
+
+export { pool };
